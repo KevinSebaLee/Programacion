e7f59810de2bad29d3eaa748bb888291930a0b24
KevinSebaLee <48044115@est.ort.edu.ar>
2024-05-08 08:34:26 -0300

    Push parte del TP 3

---
+++ TP_03-main/Clientes.cs
@@ -0,0 +1,8 @@
+class Clientes{
+    public int DNI {get; private set;}
+    public string Nombre {get; private set;}
+    public string Apellido {get; private set;}
+    public double FechaInscripcion{get; set;}
+    public int TipoEntrada{get; set;}
+    public int Cantidad{get; set;}
+}
